86c97c2744e0e5c77707115236e3c848158c7516
frogindreams <frogharvard@gmail.com>
2022-03-17 00:48:46 +0700

    the bag with the vector was yet remained

--- src/boxing/BoxMachine.h
+++ src/boxing/BoxMachine.h
@@ -1,16 +1,20 @@
-/*
- * a BoxMachine class
- */
-#include <string>
-
-using std::string;
-using std::is_same;
+#include <vector>
+#include <iostream>
 
 #ifndef BOXMACHINE_H
 #define BOXMACHINE_H
 
-/*
- * here's something
- */
+std::vector<int> vain_sequence;
+
+void packSequence(vector<int> cut_q, int size_of_seq) {
+    /*
+    vain_sequnce(size_of_seq, 0);
+    for (elem : vain_sequence) {
+        std::cout << elem << '\n';
+    }
+    */
+
+    std::cout << "what the heck" << '\n';
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
-#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <bits/stdc++.h>
 #include "boxing/BoxMachine.h"
 
 int main() {
-    unsigned long long how_many;
-    std::cin >> how_many;
+    int useless_n;
+    int size_of_seq;
+    std::cin >> useless_n;
+    size_of_seq = useless_n >> 1000;
+    int sequence[size_of_seq];
 
-    int preparation[how_many >> 1000];
-
-    for (int iter = 0; iter < how_many; ++iter) {
-        preparation[iter] = rand() % 17;
+    for (int iterX = 0; iterX < (size_of_seq - 1); ++iterX) {
+        sequence[iterX] = rand() % 18;
     }
+    std::vector<int> cut_quickly( sequence, sequence + (size_of_seq - 1) );
+    std::sort(cut_quickly.begin(), cut_quickly.end());
 
-    /* inboxing */
-    for (int iter = 0; iter < how_many; ++iter) {
-        // it's nothing
-    }
+    packSequence(cut_quickly, size_of_seq);
 }
